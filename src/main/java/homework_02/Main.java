784a34e73641f661d5d0041c7824176edd91938f
AlexandrMakacheev <amusamus1982@gmail.com>
2024-11-27 21:06:10 +0300

    homework 02

---
+++ src/main/java/homework_02/Main.java
@@ -0,0 +1,15 @@
+package homework_02;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Дрмашнее задание: Посчитать результат 2 * 2
+
+        int x = 2;
+        int y = 2;
+        int result = x / y;
+        System.out.println("Результат - " + result);
+
+    }
+}
